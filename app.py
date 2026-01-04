39be0e4c87a9595a84e308e310f7f2a053704565
Jeff Henrichs <jeffhenrichs37@gmail.com>
2026-01-03 21:42:03 -0500

    Able to search for pokemon (001 - 009, 025, and 026)

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 from flask import Flask, render_template, request, url_for
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient('mongodb://localhost:27017/')
+db = client['pokedex']
+collection = db['pokemon']
+
 @app.route('/')
-def home():
+def home():    
+    return render_template('index.html')
+
+
+@app.route('/search', methods=['GET'])
+def submit():
+    query = request.args.get('pokemon_name', '')
+
+    if query:
+        print(query, '<---------')
+        results = list(collection.find({
+            "$or": [
+                {"name": query}
+            ]
+        }))
+        print(results, '<<-------')
+    else:
+        print('Something went wrong!', '<---------')
+
     return render_template('index.html')
 
 if __name__ == '__main__':
