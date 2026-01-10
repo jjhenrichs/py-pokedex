c5902260e94f2645e6d73b03fcd00c8bb3bc39e3
Jeff Henrichs <jeffhenrichs37@gmail.com>
2026-01-09 21:18:36 -0500

    Able to generate autocomplete suggestions under search bar

--- static/js/autocomplete.js
+++ static/js/autocomplete.js
@@ -2,16 +2,15 @@ const searchBar = document.getElementById("search-bar");
 const ac_list = document.getElementById("autocomplete-list");
 
 searchBar.addEventListener("input", async () => {
-  console.log(searchBar.value);
-
   try {
     const response = await fetch(`/api/pokemon?q=${searchBar.value}`);
     const data = await response.json();
 
-    console.log(data, "<---------");
-
     for (let name of data) {
-      console.log(name);
+      const li = document.createElement("li");
+      li.className = "autocomplete-item";
+      li.textContent = name;
+      ac_list.appendChild(li);
     }
   } catch (err) {
     console.error("Error fetching Pokémon:", err);
