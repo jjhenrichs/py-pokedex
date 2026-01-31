a32ec25ff59a812ccb51f81f8d9cb8b6ad927709
Jeff Henrichs <jeffhenrichs37@gmail.com>
2026-01-30 20:36:56 -0500

    Able to change states with speech recognition

--- static/js/speech2text.js
+++ static/js/speech2text.js
@@ -1,14 +1,19 @@
 const input = document.getElementById("search-bar");
-const speakBtn = document.querySelector(".speaker");
+const speakBtn = document.querySelector(".fa-microphone");
 
 isRunning = false;
+previous_input_value = input.value;
 
 speakBtn.addEventListener("click", () => {
   if (!isRunning) {
     console.log("Starting speech recognition");
+    speakBtn.classList.toggle("active");
+    input.value = "Listening...";
     isRunning = true;
   } else {
     console.log("Stopping speech recognition");
+    speakBtn.classList.remove("active");
+    input.value = previous_input_value;
     isRunning = false;
   }
 });
