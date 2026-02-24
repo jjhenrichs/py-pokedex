e8b958e7fd8ac1a6479343843212fc104531a107
Jeff Henrichs <jeffhenrichs37@gmail.com>
2026-02-24 10:22:00 -0500

    Populating search bar with Pokemon name via voice

--- static/js/speech2text.js
+++ static/js/speech2text.js
@@ -4,16 +4,36 @@ const speakBtn = document.querySelector(".fa-microphone");
 isRunning = false;
 previous_input_value = input.value;
 
+const recognition = new (SpeechRecognition || webkitSpeechRecognition)();
+recognition.continuous = false;
+recognition.lang = "en-US";
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+if (!recognition) {
+  console.error("Speech Recognition API is not supported in this browser.");
+  speakBtn.disabled = true;
+}
+
 speakBtn.addEventListener("click", () => {
   if (!isRunning) {
-    console.log("Starting speech recognition");
+    console.log("Listening...");
     speakBtn.classList.toggle("active");
     input.value = "Listening...";
+    recognition.start();
     isRunning = true;
   } else {
-    console.log("Stopping speech recognition");
+    console.log("Idle");
     speakBtn.classList.remove("active");
     input.value = previous_input_value;
+    recognition.stop();
     isRunning = false;
   }
+});
+
+recognition.addEventListener("result", (event) => {
+  const transcript = event.results[0][0].transcript;
+  console.log("Transcript:", transcript);
+  input.value = transcript;
+  // previous_input_value = transcript;
 });
